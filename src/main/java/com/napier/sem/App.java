7cc0f9ed3ec4ad52c5fb0286e65707cb25ae2378
sandaruang35 <40794409@live.napier.ac.uk>
2025-09-18 11:39:23 +0630

    Added Hello world

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,9 +1,8 @@
 package com.napier.sem;
 
 public class App {
-        public static void main(String[] args)
-        {
-            System.out.println("Boo yah!");
-        }
+    public static void main(String[] args)
+    {
+        System.out.println("Boo yah!");
     }
-
+}
